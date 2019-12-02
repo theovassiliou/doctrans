d9e16b6f90309484a886eb23b4b3905e1b7eedf5
Theofanis Vassiliou-Gioles <vassiliou@web.de>
2019-12-02 15:37:21 +0100

    initial commit

---
+++ clients/client.go
@@ -0,0 +1,138 @@
+/*
+Copyright (c) 2019 Theofanis Vassiliou-Gioles
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+//go:generate protoc -I ../dtaservice --go_out=plugins=grpc:../dtaservice ../dtaservice/dtaservice.proto
+
+// Package main implements a client for DtaService.
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/jpillora/opts"
+
+	log "github.com/sirupsen/logrus"
+	pb "github.com/theovassiliou/doctrans/dtaservice"
+
+	"github.com/theovassiliou/go-eureka-client/eureka"
+	"google.golang.org/grpc"
+)
+
+//set this via ldflags (see https://stackoverflow.com/q/11354518)
+var version = ".1"
+
+// VERSION is the current version number.
+var VERSION = "0.0" + version + "-src"
+var conf = config{}
+
+type config struct {
+	FileName       string    `type:"arg" name:"file" help:" the file to be uploaded"`
+	EurekaURL      string    `help:"if set the indicated eureka server will be used to find DTA-GW"`
+	ServiceName    string    `help:"The service to be used"`
+	GatewayAddress string    `help:"Address and port of the server to connect"`
+	ListServices   bool      `help:"List all the services accessible"`
+	LogLevel       log.Level `help:"Log level, one of panic, fatal, error, warn or warning, info, debug, trace"`
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+const dtaGwID = "DE.TU-BERLIN.QDS.DTA.DTA-GW"
+
+func main() {
+
+	conf = config{
+		GatewayAddress: "",
+		ServiceName:    "DE.TU-BERLIN.QDS.DTA.COUNT",
+		EurekaURL:      "http://eureka:8761/eureka",
+	}
+
+	//parse config
+	opts.New(&conf).
+		Repo("github.com/theovassiliou/dta").
+		Version(VERSION).
+		Parse()
+
+	log.SetLevel(conf.LogLevel)
+
+	// Set up a connection to the server.
+	log.WithFields(log.Fields{"Service": "client", "Status": "main"}).Infof("Requesting service %s", conf.ServiceName)
+
+	if conf.EurekaURL != "" && conf.GatewayAddress == "" {
+		log.WithFields(log.Fields{"Service": "eureka", "Status": "main"}).Infof("   via  Eureka service at %s", conf.EurekaURL)
+
+		client := eureka.NewClient([]string{
+			conf.EurekaURL, //From a spring boot based eureka server
+			// add others servers here
+		})
+
+		// Let's find out whether we find the server that can serve this service.
+		gw, err := client.GetApplication(dtaGwID)
+		if err != nil || len(gw.Instances) <= 0 {
+			log.WithFields(log.Fields{"Service": "client", "Status": "main"}).Errorf("Couldn't find server for app %s", dtaGwID)
+		} else {
+			conf.GatewayAddress = gw.Instances[0].IpAddr + ":" + gw.Instances[0].Port.Port
+		}
+	}
+	log.WithFields(log.Fields{"Service": "client", "Status": "main"}).Infof("Contacting server %s", conf.GatewayAddress)
+
+	conn, err := grpc.Dial(conf.GatewayAddress, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewDTAServerClient(conn)
+
+	// Read content from file.
+	doc, err := ioutil.ReadFile(conf.FileName)
+	check(err)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	if conf.ListServices {
+		r, err := c.ListServices(ctx, &pb.ListServiceRequest{})
+		if err != nil {
+			log.Fatalf("could not list services: %v", err)
+		}
+
+		fmt.Println(string(strings.Join(r.GetServices(), "\n")))
+		os.Exit(0)
+	}
+
+	options := []string{}
+	r, err := c.TransformDocument(ctx, &pb.DocumentRequest{ServiceName: conf.ServiceName, FileName: conf.FileName, Document: doc, Options: options})
+	if err != nil {
+		log.Fatalf("could not transform: %v", err)
+	} else if r.GetError() != nil {
+		fmt.Println(strings.Join(r.GetError(), "\n"))
+		return
+	}
+	fmt.Println(string(r.GetTransDocument()))
+}

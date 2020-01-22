4171aceb5d95cb7dba67e8c0a4a750975c325f00
Theofanis Vassiliou-Gioles <vassiliou@web.de>
2020-01-22 15:01:59 +0100

    added external ip identification

--- start-service-environment.sh
+++ start-service-environment.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 rm -f .env
 echo "HOST=`hostname`" >> .env
+echo "IP=`/sbin/ifconfig en0 | grep "inet " | awk -F' ' '{print $2}' | awk '{print $1}'`" >> .env
 docker-compose -f docker-compose.yaml up -d 

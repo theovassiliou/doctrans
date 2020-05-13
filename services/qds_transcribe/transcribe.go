fbffc06cb79c27be87031d7b549540fb52e6be28
Theofanis Vassiliou-Gioles <vassiliou@web.de>
2020-05-13 21:00:21 +0200

    renamed pb package to dta

--- services/qds_transcribe/transcribe.go
+++ services/qds_transcribe/transcribe.go
@@ -9,7 +9,7 @@ import (
 	"github.com/mitchellh/go-homedir"
 
 	log "github.com/sirupsen/logrus"
-	pb "github.com/theovassiliou/doctrans/dtaservice"
+	dta "github.com/theovassiliou/doctrans/dtaservice"
 	aux "github.com/theovassiliou/doctrans/ipaux"
 	service "github.com/theovassiliou/doctrans/services/qds_transcribe/serviceimplementation"
 )
@@ -19,28 +19,27 @@ var version = ".1"
 // VERSION is the current version number.
 var VERSION = "0.0" + version + "-src"
 
-// TODO Splitt APP Name into appName (ECHO) and scope (DE.TU-BERLIN.QDS) so that it
-// can be configured individual
 const (
 	appName = "BERLIN.VASSILIOUTHEO.TRANSCRIBE"
 	dtaType = "Service"
 )
 
-type ServiceOptions struct {
-	pb.DocTransServerOptions
-	pb.DocTransServerGenericOptions
+type serviceCmdLineOptions struct {
+	dta.DocTransServerOptions
+	dta.DocTransServerGenericOptions
 	LocalExecution      bool   `opts:"group=Local Execution, short=x" help:"If set, execute the service locally once."`
 	LocalAdditionalInfo bool   `opts:"group=Local Execution, short=a" help:"Additional information on local execution. Otherwise ignored."`
 	LocalFileName       string `opts:"group=Local Execution, short=f" help:"media file name if executed locally, Otherwise ignored."`
+	AppName             string
 }
 
-func calcStatusURL(url, appName, instanceId string) string {
-	return url + "/apps/" + appName + "/" + instanceId
+func calcStatusURL(url, appName, instanceID string) string {
+	return url + "/apps/" + appName + "/" + instanceID
 }
 
-func NewDtaService(options ServiceOptions, appName, proto string) service.DtaService {
+func newDtaService(options serviceCmdLineOptions, appName, proto string) service.DtaService {
 	var gw = service.DtaService{
-		DocTransServer: pb.DocTransServer{
+		DocTransServer: dta.DocTransServer{
 			AppName: appName,
 			DtaType: dtaType,
 			Proto:   proto,
@@ -53,7 +52,7 @@ func main() {
 	workingHomeDir, _ := homedir.Dir()
 	homepageURL := "https://github.com/theovassiliou/doctrans/blob/master/gateway/README.md"
 
-	serviceOptions := ServiceOptions{}
+	serviceOptions := serviceCmdLineOptions{}
 	serviceOptions.CfgFile = workingHomeDir + "/.dta/" + appName + "/config.json"
 	serviceOptions.LogLevel = log.WarnLevel
 	serviceOptions.HostName = aux.GetHostname()
@@ -86,26 +85,26 @@ func main() {
 	// -- take initial port
 	_initialPort := serviceOptions.Port
 	// -- start listener and save used grpc port
-	_grpcListener, _grpcPort := pb.InitListener(_initialPort)
+	_grpcListener, _grpcPort := dta.InitListener(_initialPort)
 
 	// create HTTP Listener (optional)
 	if registerHTTP {
 		// -- take GRPC port + 1
 		// -- start listener and save used http port
-		_httpListener, _httpPort = pb.InitListener(_grpcPort + 1)
+		_httpListener, _httpPort = dta.InitListener(_grpcPort + 1)
 	}
 
-	ipAddressUsed, _ := aux.ExternalIP()
+	_ipAddressUsed, _ := aux.ExternalIP()
 
-	grpcGateway := NewDtaService(serviceOptions, appName, "grpc")
-	grpcGateway.NewInstanceInfo("grpc@"+serviceOptions.HostName, appName, ipAddressUsed, _grpcPort,
+	grpcGateway := newDtaService(serviceOptions, appName, "grpc")
+	grpcGateway.NewInstanceInfo("grpc@"+serviceOptions.HostName, appName, _ipAddressUsed, _grpcPort,
 		0, false, dtaType, "grpc",
 		homepageURL,
 		calcStatusURL(serviceOptions.RegistrarURL, appName, "grpc@"+serviceOptions.HostName),
 		"")
 
-	httpGateway := NewDtaService(serviceOptions, appName, "http")
-	httpGateway.NewInstanceInfo("http@"+serviceOptions.HostName, appName, ipAddressUsed, _httpPort,
+	httpGateway := newDtaService(serviceOptions, appName, "http")
+	httpGateway.NewInstanceInfo("http@"+serviceOptions.HostName, appName, _ipAddressUsed, _httpPort,
 		0, false, dtaType, "http",
 		homepageURL,
 		calcStatusURL(serviceOptions.RegistrarURL, appName, "http@"+serviceOptions.HostName),
@@ -120,8 +119,8 @@ func main() {
 		grpcGateway.RegisterAtRegistry(serviceOptions.RegistrarURL)
 	}
 	if registerGRPC {
-		go pb.StartGrpcServer(_grpcListener, &grpcGateway)
-		pb.CaptureSignals(&grpcGateway.DocTransServer, serviceOptions.RegistrarURL, &wg)
+		go dta.StartGrpcServer(_grpcListener, &grpcGateway)
+		dta.CaptureSignals(&grpcGateway.DocTransServer, serviceOptions.RegistrarURL, &wg)
 		wg.Add(1)
 	}
 
@@ -134,8 +133,8 @@ func main() {
 		ctx := context.Background()
 		ctx, cancel := context.WithCancel(ctx)
 		defer cancel()
-		go pb.MuxHTTPGrpc(ctx, _httpListener, _grpcPort)
-		pb.CaptureSignals(&httpGateway.DocTransServer, serviceOptions.RegistrarURL, &wg)
+		go dta.MuxHTTPGrpc(ctx, _httpListener, _grpcPort)
+		dta.CaptureSignals(&httpGateway.DocTransServer, serviceOptions.RegistrarURL, &wg)
 		wg.Add(1)
 	}
 
@@ -143,7 +142,7 @@ func main() {
 	return
 }
 
-func determineServerConfig(gwOptions ServiceOptions) (registerGRPC, registerHTTP bool) {
+func determineServerConfig(gwOptions serviceCmdLineOptions) (registerGRPC, registerHTTP bool) {
 	if (!gwOptions.HTTP && !gwOptions.GRPC) || gwOptions.GRPC {
 		registerGRPC = true
 	}

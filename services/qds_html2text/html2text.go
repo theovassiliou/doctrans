7220b246d0538a9c6fc1ea8f8a5a1b411f58537b
Theofanis Vassiliou-Gioles <vassiliou@web.de>
2020-08-27 13:10:06 +0200

    moved to new implementation structure.

--- services/qds_html2text/html2text.go
+++ services/qds_html2text/html2text.go
@@ -1,20 +1,14 @@
 package main
 
+// A simple implemenation of using the Golang DocTrans Framework
 import (
-	"context"
-	"net"
-	"sync"
-
 	"github.com/jpillora/opts"
 	"github.com/mitchellh/go-homedir"
-	"github.com/theovassiliou/go-eureka-client/eureka"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
-	"jaytaylor.com/html2text"
 
 	log "github.com/sirupsen/logrus"
-	pb "github.com/theovassiliou/doctrans/dtaservice"
+	dta "github.com/theovassiliou/doctrans/dtaservice"
 	aux "github.com/theovassiliou/doctrans/ipaux"
+	service "github.com/theovassiliou/doctrans/services/qds_html2text/serviceimplementation"
 )
 
 var version = ".1"
@@ -27,46 +21,17 @@ const (
 	dtaType = "Service"
 )
 
-// Work returns a nicely formatted text from a HTML input
-func Work(input []byte, options []string) (string, []string, error) {
-	text, err := html2text.FromString(string(input), html2text.Options{PrettyTables: true})
-	return string(text), []string{}, err
-}
-
-type ServiceOptions struct {
-	pb.DocTransServerOptions
-	pb.DocTransServerGenericOptions
-}
-
-func calcStatusURL(url, appName, instanceId string) string {
-	return url + "/apps/" + appName + "/" + instanceId
-}
-
-func NewDtaService(options ServiceOptions, appName, proto string) pb.IDocTransServer {
-	var gw = DtaService{
-		GenDocTransServer: pb.GenDocTransServer{
-			AppName: appName,
-			DtaType: dtaType,
-			Proto:   proto,
-		},
-	}
-	return &gw
-}
-
-// DtaService holds the infrastructure for performing the service.
-type DtaService struct {
-	pb.UnimplementedDTAServerServer
-	pb.GenDocTransServer
-	resolver *eureka.Client
-	listener net.Listener
-	pb.IDocTransServer
+type serviceCmdLineOptions struct {
+	dta.DocTransServerOptions
+	dta.DocTransServerGenericOptions
+	LocalExecution string `opts:"group=Local Execution, short=x" help:"If set, execute the service locally once and read from this file"`
 }
 
 func main() {
 	workingHomeDir, _ := homedir.Dir()
 	homepageURL := "https://github.com/theovassiliou/doctrans/blob/master/gateway/README.md"
 
-	serviceOptions := ServiceOptions{}
+	serviceOptions := serviceCmdLineOptions{}
 	serviceOptions.CfgFile = workingHomeDir + "/.dta/" + appName + "/config.json"
 	serviceOptions.LogLevel = log.WarnLevel
 	serviceOptions.HostName = aux.GetHostname()
@@ -82,76 +47,45 @@ func main() {
 		log.SetLevel(serviceOptions.LogLevel)
 	}
 
+	if serviceOptions.LocalExecution != "" {
+		s := service.DtaService{}
+		s.AppName = appName
+
+		service.ExecuteWorkerLocally(s, serviceOptions.LocalExecution)
+		return
+	}
+
+	var _grpcGateway, _httpGateway dta.IDocTransServer
 	// Calc Configuration
 	registerGRPC, registerHTTP := determineServerConfig(serviceOptions)
-
-	var _httpListener net.Listener
-	var _httpPort int
-
-	// create GRPC Listener
-	// -- take initial port
-	_initialPort := serviceOptions.Port
-	// -- start listener and save used grpc port
-	_grpcListener, _grpcPort := pb.InitListener(_initialPort)
-
-	// create HTTP Listener (optional)
-	if registerHTTP {
-		// -- take GRPC port + 1
-		// -- start listener and save used http port
-		_httpListener, _httpPort = pb.InitListener(_grpcPort + 1)
-	}
-
-	ipAddressUsed, _ := aux.ExternalIP()
-
-	grpcGateway := NewDtaService(serviceOptions, appName, "grpc")
-	gDTS := grpcGateway.GetDocTransServer()
-	gDTS.NewInstanceInfo("grpc@"+serviceOptions.HostName, appName, ipAddressUsed, _grpcPort,
-		0, false, dtaType, "grpc",
-		homepageURL,
-		calcStatusURL(serviceOptions.RegistrarURL, appName, "grpc@"+serviceOptions.HostName),
-		"")
-
-	httpGateway := NewDtaService(serviceOptions, appName, "http")
-	hDTS := httpGateway.GetDocTransServer()
-	hDTS.NewInstanceInfo("http@"+serviceOptions.HostName, appName, ipAddressUsed, _httpPort,
-		0, false, dtaType, "http",
-		homepageURL,
-		calcStatusURL(serviceOptions.RegistrarURL, appName, "http@"+serviceOptions.HostName),
-		"")
-
-	var wg sync.WaitGroup
-
-	// Register at registrar
-	// -- Register service with GRPC protocol
-	log.Tracef("RegistrarURL: %s\n", serviceOptions.RegistrarURL)
-	if registerGRPC && serviceOptions.RegistrarURL != "" {
-		gDTS.RegisterAtRegistry(serviceOptions.RegistrarURL)
-	}
 	if registerGRPC {
-		go pb.StartGrpcServer(_grpcListener, grpcGateway)
-		pb.CaptureSignals(grpcGateway, serviceOptions.RegistrarURL, &wg)
-		wg.Add(1)
+		_grpcGateway = newDtaService(serviceOptions, appName, "grpc")
 	}
-
-	// -- Register service with HTTP protocol (optional)
-	if registerHTTP && serviceOptions.RegistrarURL != "" {
-		hDTS.RegisterAtRegistry(serviceOptions.RegistrarURL)
-	}
-
 	if registerHTTP {
-		ctx := context.Background()
-		ctx, cancel := context.WithCancel(ctx)
-		defer cancel()
-		go pb.MuxHTTPGrpc(ctx, _httpListener, _grpcPort)
-		pb.CaptureSignals(httpGateway, serviceOptions.RegistrarURL, &wg)
-		wg.Add(1)
+		_httpGateway = newDtaService(serviceOptions, appName, "http")
 	}
 
-	wg.Wait()
+	dta.LaunchServices(_grpcGateway, _httpGateway, appName, dtaType, homepageURL, serviceOptions.DocTransServerOptions)
+
 	return
 }
 
-func determineServerConfig(gwOptions ServiceOptions) (registerGRPC, registerHTTP bool) {
+func newDtaService(options serviceCmdLineOptions, appName, proto string) dta.IDocTransServer {
+	var gw service.DtaService
+
+	gw = service.DtaService{
+		GenDocTransServer: dta.GenDocTransServer{
+			AppName: appName,
+			DtaType: dtaType,
+			Proto:   proto,
+		},
+	}
+	gw.AppName = appName
+
+	return &gw
+}
+
+func determineServerConfig(gwOptions serviceCmdLineOptions) (registerGRPC, registerHTTP bool) {
 	if (!gwOptions.HTTP && !gwOptions.GRPC) || gwOptions.GRPC {
 		registerGRPC = true
 	}
@@ -160,43 +94,4 @@ func determineServerConfig(gwOptions ServiceOptions) (registerGRPC, registerHTTP
 		registerHTTP = true
 	}
 	return
-}
-
-// TransformDocument implements dtaservice.DTAServer
-func (s *DtaService) TransformDocument(ctx context.Context, docReq *pb.DocumentRequest) (*pb.TransformDocumentResponse, error) {
-	transResult, stdOut, stdErr := Work(docReq.GetDocument(), docReq.GetOptions())
-	var errorS []string = []string{}
-	if stdErr != nil {
-		errorS = []string{stdErr.Error()}
-	}
-	log.WithFields(log.Fields{"Service": s.ApplicationName(), "Status": "TransformDocument"}).Tracef("Received document: %s", string(docReq.GetDocument()))
-	log.WithFields(log.Fields{"Service": s.ApplicationName(), "Status": "TransformDocument"}).Tracef("Transformation Result: %s", transResult)
-
-	return &pb.TransformDocumentResponse{
-		TransDocument: []byte(transResult),
-		TransOutput:   stdOut,
-		Error:         errorS,
-	}, nil
-}
-
-// ListServices list available services provided by this implementation
-func (s *DtaService) ListServices(ctx context.Context, req *pb.ListServiceRequest) (*pb.ListServicesResponse, error) {
-	log.WithFields(log.Fields{"Service": s.ApplicationName(), "Status": "ListServices"}).Tracef("Service requested")
-	log.WithFields(log.Fields{"Service": s.ApplicationName(), "Status": "ListServices"}).Infof("In know only myself: %s", s.ApplicationName())
-	services := (&pb.ListServicesResponse{}).Services
-	services = append(services, s.ApplicationName())
-	return &pb.ListServicesResponse{Services: services}, nil
-
-}
-
-func (*DtaService) TransformPipe(context.Context, *pb.TransformPipeRequest) (*pb.TransformDocumentResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method TransformPipe not implemented")
-}
-func (*DtaService) Options(context.Context, *pb.OptionsRequest) (*pb.OptionsResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method Options not implemented")
-}
-
-// ApplicationName returns the name of the service application
-func (s *DtaService) ApplicationName() string {
-	return s.AppName
 }
